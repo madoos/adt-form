0b653b3e8db9e79c2e167589285b7cee541b04a8
Maurice Domínguez <maurice.ronet.dominguez@gmail.com>
2020-11-13 19:30:14 +0100

    refactor: change validations to either type

--- src/index.js
+++ src/index.js
@@ -2,6 +2,34 @@ import * as api from './api'
 import ui from './ui-ids'
 import { isAlphanumeric, isLengthGte5, isLengthLte8 } from './validations'
 
+import { pipe, curry, chain, pipeK, sequence } from 'ramda'
+import { Either, IO } from 'crocks'
+import { Map  } from 'immutable-ext'
+
+////////////// validations
+
+// validateWith :: (a -> Boolean) -> String -> a -> Either String a
+const validateWith = curry((predicate, msg, x) => {
+    return predicate(x) ? Either.of(x) : Either.Left(msg)
+})
+
+
+// validateUserName :: User -> Either String User
+const validateUserName = validateWith(({ user }) => isLengthGte5(user), 'User must to be size greater than or equal 5.')
+
+// validatePasswordAlpha :: User -> Either String User
+const validatePasswordAlpha = validateWith(({ password }) => isAlphanumeric(password), 'Password must to be alphanumeric ')
+
+// validateSize :: User -> Either String User
+const validateSize = validateWith(({ password }) => (isLengthGte5(password) && isLengthLte8(password)), 'Password must to be a word with size between 5 and 8.')
+
+// validateUser :: User -> Either String User
+const validateUser = pipeK(
+    validateUserName,
+    validatePasswordAlpha,
+    validateSize
+)
+
 document.addEventListener('DOMContentLoaded',() => {
     const userInput = document.getElementById(ui.user)
     const passwordInput = document.getElementById(ui.password)
@@ -16,19 +44,18 @@ document.addEventListener('DOMContentLoaded',() => {
     enterBtn.addEventListener('click', () => {
         const user = userInput.value
         const password = passwordInput.value
-        
-        if (!isLengthGte5(user)) {
-            notificationParagraph.innerHTML = 'User must to be size greater than or equal 5.'
-            cleanForm()
-        }
-        else if (!isAlphanumeric(password) || !(isLengthGte5(password) && isLengthLte8(password)) ) {
-            notificationParagraph.innerHTML = 'Password must to be alphanumeric word with size between 5 and 8.'
-            cleanForm()
 
-        }else {
-            api.login(user, password)
-            .then(user => notificationParagraph.innerHTML = `Welcome ${user.name}!`)
-            .then(() => cleanForm())
-        }
+        validateUser({ user, password })
+            .either(
+                msg => {
+                    notificationParagraph.innerHTML = msg
+                    cleanForm()
+                }, 
+                ({ user, password }) => {
+                    api.login(user, password)
+                    .then(user => notificationParagraph.innerHTML = `Welcome ${user.name}!`)
+                    .then(() => cleanForm())
+                }
+            )
     })
 })
